c6fc2f41330cf36c3aca505e91ddde2928644f59
luxeivan <luxeivan@gmail.com>
2022-05-30 19:02:14 +0600

    completed3

--- src/js/app.js
+++ src/js/app.js
@@ -3,23 +3,22 @@ import img from '../img/goblin.png';
 const gridElem = document.querySelectorAll('.grid__elem');
 const image = document.createElement('img');
 let currentNumberElem = 0;
-let arrayElem = [...gridElem];
+const arrayElem = [...gridElem];
 image.className = 'image';
 image.src = img;
 
 function generateNumberElem() {
   const min = 0;
   const max = 15;
-  let numberElem = Math.floor(Math.random() * (max - min + 1)) + min;
-  if (currentNumberElem != numberElem) {
+  const numberElem = Math.floor(Math.random() * (max - min + 1)) + min;
+  if (currentNumberElem !== numberElem) {
     currentNumberElem = numberElem;
   } else {
     generateNumberElem();
   }
 }
 
-setInterval(()=>{
+setInterval(() => {
   generateNumberElem();
   arrayElem[currentNumberElem].insertBefore(image, null);
-},1000);
-
+}, 1000);
